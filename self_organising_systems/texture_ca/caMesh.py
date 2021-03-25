d59ec002413a4541335700231d1f6d99558d09b2
Bathsheba <b@bathsheba.com>
2021-03-25 16:22:21 -0400

    Add caMesh.py for working on meshes, parallel to ca.py
    Signed-off-by: Bathsheba <b@bathsheba.com>

--- self_organising_systems/texture_ca/caMesh.py
+++ self_organising_systems/texture_ca/caMesh.py
@@ -1,7 +1,6 @@
 # Lint as: python3
 """
 Cellular Automata Model, adapted to run a  on a closed mesh.
-helloo
 """
 from self_organising_systems.texture_ca.config import cfg
 import tensorflow as tf
